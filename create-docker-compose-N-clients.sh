e6f311711c74d2e49c2368dcae4cc387e685d982
MauricioBuzzone <mbuzzone@fi.uba.ar>
2023-08-28 15:36:15 -0300

    ejericio 2: los archivos de configuracion estan dentro de los volumenes

--- create-docker-compose-N-clients.sh
+++ create-docker-compose-N-clients.sh
@@ -24,6 +24,8 @@ services:
     environment:
       - PYTHONUNBUFFERED=1
       - LOGGING_LEVEL=DEBUG
+    volumes:
+      - ./server/config.ini:/config.ini
     networks:
       - testing_net" > "$file_name"
 
@@ -35,6 +37,8 @@ for ((i = 1; i <= $1; i++)); do
     environment:
       - CLI_ID=$i
       - CLI_LOG_LEVEL=DEBUG
+    volumes:
+      - ./client/config.yaml:/config.yaml
     networks:
       - testing_net
     depends_on:
